8a2820f716b75353a2b1cfe7ea1c827bf70c30a5
Wagner <wagaocarvalhodev@gmail.com>
2023-05-24 21:29:40 -0300

    feat: .env and .gitignore

--- main.go
+++ main.go
@@ -4,28 +4,25 @@ import (
 	"database/sql"
 	"log"
 
-	//"os"
+	"os"
 
 	"github.com/WagaoCarvalhoDev/gofinance-backend/api"
 	db "github.com/WagaoCarvalhoDev/gofinance-backend/db/sqlc"
 
-	//"github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	// dbDriver := os.Getenv("DB_DRIVER")
-	// dbSource := os.Getenv("DB_SOURCE")
-	// serverAddress := os.Getenv("SERVER_ADDRESS")
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
 
-	dbDriver := "postgres"
-	dbSource := "postgresql://super_admin:SomeSecretPassword@localhost:5432/go_finance?sslmode=disable"
-	serverAddress := "0.0.0.0:8000"
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
